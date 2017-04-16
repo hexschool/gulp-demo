1f465ac4cbb2b6b9af46485fac712cde0b2b5214
wcc723 <agito723@gmail.com>
2017-04-16 11:07:58 +0800

    加入 load-plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,6 @@
 
-var gulp = require('gulp'),
-  sass = require('gulp-sass'),
-  postcss = require('gulp-postcss'),
-  autoprefixer = require('autoprefixer'),
-  jade = require('gulp-jade'),
-  plumber = require('gulp-plumber');
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
 
 gulp.task('copyHtml', function(){
   return gulp.src(['./source/**/*.html'])
@@ -13,16 +9,16 @@ gulp.task('copyHtml', function(){
 
 gulp.task('jade', function(){
   return gulp.src(['./source/**/*.jade'])
-    .pipe(plumber())
-    .pipe(jade({ pretty: true }))
+    .pipe($.plumber())
+    .pipe($.jade({ pretty: true }))
     .pipe(gulp.dest('./public'));
 });
 
 gulp.task('sass', function(){
   return gulp.src(['./source/stylesheets/**/*.sass', './source/stylesheets/**/*.scss'])
-    .pipe(plumber())
-    .pipe(sass({outputStyle: 'nested'})
-      .on('error', sass.logError))
+    .pipe($.plumber())
+    .pipe($.sass({outputStyle: 'nested'})
+      .on('error', $.sass.logError))
     .pipe(gulp.dest('./public/stylesheets'));
 })
 
