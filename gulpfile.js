15817fbe654cfb7435256ff34f9a2d34c6febd25
wcc723 <agito723@gmail.com>
2017-11-01 11:33:38 +0800

    加入 reloadDebounce

--- gulpfile.js
+++ gulpfile.js
@@ -114,7 +114,8 @@ gulp.task('imageMin', function () {
 
 gulp.task('browserSync', function () {
   browserSync.init({
-    server: { baseDir: './public' }
+    server: { baseDir: './public' },
+    reloadDebounce: 2000
   })
 });
 
